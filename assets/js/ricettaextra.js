5ae3e94fe33ba9f9aaeca8efdfa3ed3af409ef21
Marco Pala <marcopala1994@gmail.com>
2024-01-22 16:22:16 +0100

    U1-W2-D1-js

--- assets/js/ricettaextra.js
+++ assets/js/ricettaextra.js
@@ -1,10 +1,25 @@
+/*Per preparare la torta di mele come prima cosa sbucciate le mele 1, tagliatele a spicchi ed eliminate il torsolo.
+ Tagliatele poi a fettine sottili dal lato corto 2. In una ciotola versate le uova e lo zucchero 3.
+*/
 let mele = 8;
-let farina = 250;
-let zucchero = 250;
-let latte = 150;
-let limone = 1;
+/*Montate con le fruste elettriche e non appena
+ il composto sarà chiaro e spumoso aggiungete un pizzico di sale . Versate poi anche il burro tiepido 
+*/
 let uova = 2;
 let burro = 100;
+/*Aggiungete la scorza di limone grattugiata 7 e sbattete ancora 8. Setacciate all'interno la farina*/
+let farina = 250;
+let limone = 1;
+/*Setacciate anche il lievito 10 e mescolate ancora
+ con le fruste, aggiungendo il latte lentamente 11. Dovrete ottenere un composto liscio e cremoso 12.
+*/
+let zucchero = 250;
+let latte = 150;
+/*Versate all'interno le mele 13 e mescolate bene 14. Non preoccupatevi se
+ sembrano molte. Trasferite il composto all'interno di uno stampo da 22 cm imburrata e cosparsa di zucchero 15.
+*/
 let risultato = farina + zucchero + uova + mele;
-console.log(risultato + "grammi totali neccessari");
+let liquidi = latte + limone;
+console.log(risultato + "grammi totali neccessari per preparare questa fantastica torta");
+console.log(liquidi + "ml totali neccessari per preparare questa fantastica torta");
 window.alert("Pericolo Calorie");
